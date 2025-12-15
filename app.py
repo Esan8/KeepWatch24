34347e9092d9b27f63eb42f34287e7da86a4d56b
TheWatch <161582347+Esan8@users.noreply.github.com>
2025-12-15 15:10:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ DAILY_ENGAGEMENT_MULTIPLIER = 1.2
 
 # Other constants remain the same
 DAU_START_DATE_STR = "2025-05-16"
-DAU_END_DATE_STR = "2025-12-14"
+DAU_END_DATE_STR = "2025-12-15"
 
 # ===========================
 # 3. App Constants & Patterns
